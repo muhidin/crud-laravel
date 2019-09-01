026d179af2870247d149f5806dcd930c5f824002
Muhidin Saimin <muhidins@gmail.com>
2019-09-01 08:09:26 +0700

    upload crud-laravel

---
+++ app/Buku.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Buku extends Model
+{
+    public $table = "buku";
+    //protected $primaryKey = 'kdbuku';
+    protected $guarded = [];
+}
